dbbaf920ac71fd2cd5b0725e595d4facd65fe002
Samuel Wilson <samWson@users.noreply.github.com>
2023-12-15 10:02:24 +1300

    Make a functioning unit test
    Now start work on making the module that creates the maze. Use unit
    tests for development. Display the result in the web page.

---
+++ test/mazes_for_programmers/binary_tree_test.exs
@@ -0,0 +1,7 @@
+defmodule MazesForProgrammers.BinaryTreeTest do
+  use ExUnit.Case, async: true
+
+  test "says hello" do
+    assert MazesForProgrammers.BinaryTree.hello() == "Hello. This is where the maze goes."
+  end
+end
